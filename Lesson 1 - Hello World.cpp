677606fccb7f3b1daa70af5f2263c0eecb3b5f5d
Albert <awxchang@gmail.com>
2018-03-16 21:42:30 -0400

    Albert

---
+++ Lesson 1 - Hello World.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "hello world";
+
+	return 0;
+}
+
+/*
+1. "<iostream>" is a header file that contains a list of functions necessary
+such as cout etc. The #include is the preprocessor that tells the program to
+include this  header file.
+
+2.Namespaces are used to organize code into logical groups and to prevent name
+collisions that can occur especially when your code base includes multiple
+libraries.
+
+3.Note: When coding, it's always crucial to keep in the back of your mind to
+leave comments which will help fellow programmers follow through your code when
+working in collaborations or projects.
+
+*/
